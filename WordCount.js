a1e5dd7699354d8728dca332d423296768da41b4
imiqau <153656435+imiqau@users.noreply.github.com>
2024-01-30 00:32:05 +0500

    Add files via upload

---
+++ WordCount.js
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+let str = "Yes";
+while (str == "Yes") {
+    let para = await inquirer.prompt({
+        type: "input",
+        name: "abc",
+        message: "please enter your paragraph:"
+    });
+    let wordCnt = 0;
+    let alphaCnt = 0;
+    for (let i = 0; i < para.abc.length; i++) {
+        const element = para.abc[i];
+        if (para.abc[i] === " " && para.abc[i - 1] != " " && i > 1) {
+            wordCnt = wordCnt + 1;
+        }
+        else if (para.abc[i] != " " && para.abc[i] != "." && para.abc[i] != "," && para.abc[i] != "'") {
+            alphaCnt = alphaCnt + 1;
+        }
+    }
+    wordCnt = wordCnt + 1;
+    console.log(`There are total of ${wordCnt} words and ${alphaCnt} letters in the paragraph`);
+    let user = await inquirer.prompt({
+        type: "list",
+        name: "ask",
+        message: "Would you like to write another paragraph? ",
+        choices: ['Yes', 'No']
+    });
+    str = user.ask;
+}
